ce89d8d17440bbca8790db1dc7fbf5185e475a97
hawkfalcon <contact@hawkfalcon.com>
2018-05-13 15:24:49 -0700

    Stub for potential future project

---
+++ JunimoRail/JunimoRail.cs
@@ -0,0 +1,17 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Quests;
+using System.Linq;
+using xTile.Layers;
+
+namespace JunimoRail {
+	public class JunimoRail : Mod {
+      
+		public override void Entry(IModHelper helper) {
+
+		}
+   
+	}
+}
---
+++ JunimoRail/ModConfig.cs
@@ -0,0 +1,6 @@
+﻿namespace JunimoRail {
+    class ModConfig {
+        public bool NeverExpires { get; set; } = false;
+        public int DaysToExpiration { get; set; } = 3;
+    }
+}
