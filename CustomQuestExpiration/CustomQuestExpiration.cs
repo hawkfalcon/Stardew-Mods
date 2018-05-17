805dd2e635b8ce9ff93c96929b52efa2ae30960c
hawkfalcon <contact@hawkfalcon.com>
2018-05-16 20:57:38 -0700

    Fix NeverExpires breaking some quests.

--- CustomQuestExpiration/CustomQuestExpiration.cs
+++ CustomQuestExpiration/CustomQuestExpiration.cs
@@ -2,36 +2,65 @@
 using StardewModdingAPI.Events;
 using StardewValley;
 using StardewValley.Quests;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CustomQuestExpiration {
     public class CustomQuestExpiration : Mod {
+        private const int InfiniteDays = 100000;
         private ModConfig Config;
+        private List<Quest> dailyQuests = new List<Quest>();
        
         public override void Entry(IModHelper helper) {
-            this.Config = Helper.ReadConfig<ModConfig>();
-            
+            Config = Helper.ReadConfig<ModConfig>();
+
+            if (Config.NeverExpires) {
+                MenuEvents.MenuChanged += MenuEvents_MenuChanged;
+            }
             MenuEvents.MenuClosed += MenuEvents_MenuClosed;
+        }
+
+        private void MenuEvents_MenuChanged(object sender, EventArgsClickableMenuChanged e) {
+            if (e.NewMenu is StardewValley.Menus.QuestLog) {
+                updateQuestIcons();
+            }
         }
       
         private void MenuEvents_MenuClosed(object sender, EventArgsClickableMenuClosed e) {
-            // Only update days left when leaving the billboard
             if (e.PriorMenu is StardewValley.Menus.Billboard) {
                 updateQuest();
             }
+            else if (Config.NeverExpires && e.PriorMenu is StardewValley.Menus.QuestLog) {
+                resetQuestIcons();
+            }
         }
 
+        // Update quests when leaving the billboard
         private void updateQuest() {
             Quest currentDailyQuest = Game1.player.questLog.FirstOrDefault(quest =>
-                quest.dailyQuest && quest.Equals(Game1.questOfTheDay));
-            if (currentDailyQuest != null) {             
-                if (Config.NeverExpires) {
-                    currentDailyQuest.dailyQuest.Value = false;
-                }
-                else {
-                    currentDailyQuest.daysLeft.Value = Config.DaysToExpiration;
+                quest.dailyQuest && quest.Equals(Game1.questOfTheDay)
+            );
+            if (currentDailyQuest != null) {
+                int daysLeft = Config.NeverExpires ? InfiniteDays : Config.DaysToExpiration;
+                currentDailyQuest.daysLeft.Value = daysLeft;
+            }
+        }
+
+        // Make all quests not daily temporarily to set the icon to (!)
+        private void updateQuestIcons() {
+            foreach (Quest quest in Game1.player.questLog) {
+                if (quest.dailyQuest) {
+                    dailyQuests.Add(quest);
+                    quest.dailyQuest.Value = false;
                 }
             }
+        }
+
+        private void resetQuestIcons() {
+            foreach (Quest quest in dailyQuests) {
+                quest.dailyQuest.Value = true;
+            }
+            dailyQuests.Clear();
         }
     }
 }
