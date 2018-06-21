3d0e392626f652d7253274730ed73ffd50187e85
hawkfalcon <contact@hawkfalcon.com>
2018-06-21 10:31:20 -0700

    CheckSpeedGro

--- BetterJunimos/Utils/JunimoAbilities.cs
+++ BetterJunimos/Utils/JunimoAbilities.cs
@@ -173,7 +173,9 @@ namespace BetterJunimos.Patches {
 
             Farm farm = Game1.getFarm();
             if (farm.terrainFeatures[pos] is HoeDirt hd) {
+                CheckSpeedGro(hd, crop);
                 hd.crop = crop;
+
                 if (Utility.isOnScreen(Utility.Vector2ToPoint(pos), 64, farm)) {
                     if (crop.raisedSeeds)
                         farm.playSound("stoneStep");
@@ -183,6 +185,28 @@ namespace BetterJunimos.Patches {
                 ++Game1.stats.SeedsSown;
             }
             return true;
+        }
+
+        private void CheckSpeedGro(HoeDirt hd, Crop crop) {
+            if (hd.fertilizer.Value == 465 || hd.fertilizer.Value == 466 || Game1.player.professions.Contains(5)) {
+                int num1 = 0;
+                for (int index1 = 0; index1 < crop.phaseDays.Count - 1; ++index1)
+                    num1 += crop.phaseDays[index1];
+                float num2 = hd.fertilizer.Value == 465 ? 0.1f : (hd.fertilizer.Value == 466 ? 0.25f : 0.0f);
+                if (Game1.player.professions.Contains(5))
+                    num2 += 0.1f;
+                int num3 = (int)Math.Ceiling((double)num1 * (double)num2);
+                for (int index1 = 0; num3 > 0 && index1 < 3; ++index1) {
+                    for (int index2 = 0; index2 < crop.phaseDays.Count; ++index2) {
+                        if (index2 > 0 || crop.phaseDays[index2] > 1) {
+                            crop.phaseDays[index2]--;
+                            --num3;
+                        }
+                        if (num3 <= 0)
+                            break;
+                    }
+                }
+            }
         }
     }
 }
