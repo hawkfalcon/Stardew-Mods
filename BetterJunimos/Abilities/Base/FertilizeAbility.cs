d8b16c6ef2970a694bb7a2236b36ed145c98b777
ceruleandeep <ceruleandeep>
2022-02-24 16:36:11 +1100

    Apply correct bonus for Hyper SpeedGro

--- BetterJunimos/Abilities/Base/FertilizeAbility.cs
+++ BetterJunimos/Abilities/Base/FertilizeAbility.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Linq;
 using System.Collections.Generic;
 using BetterJunimos.Utils;
@@ -46,9 +46,55 @@ namespace BetterJunimos.Abilities {
         private void Fertilize(Farm farm, Vector2 pos, int index) {
             if (farm.terrainFeatures[pos] is HoeDirt hd) {
                 hd.fertilizer.Value = index;
+                CheckSpeedGro(hd, hd.crop);
                 if (Utility.isOnScreen(Utility.Vector2ToPoint(pos), 64, farm)) {
                     farm.playSound("dirtyHit");
                 }
+            }
+        }
+
+        // taken from SDV planting code [applySpeedIncreases()], updated for 1.5
+        protected void CheckSpeedGro(HoeDirt hd, Crop crop) {
+            int fertilizer = hd.fertilizer.Value;
+            Farmer who = Game1.player;
+
+            if (crop == null) {
+                return;
+            }
+            if (!(((int)fertilizer == 465 || (int)fertilizer == 466 || (int)fertilizer == 918 || who.professions.Contains(5)))) {
+                return;
+            }
+            crop.ResetPhaseDays();
+            int totalDaysOfCropGrowth = 0;
+            for (int j = 0; j < crop.phaseDays.Count - 1; j++) {
+                totalDaysOfCropGrowth += crop.phaseDays[j];
+            }
+            float speedIncrease = 0f;
+            if ((int)fertilizer == 465) {
+                speedIncrease += 0.1f;
+            }
+            else if ((int)fertilizer == 466) {
+                speedIncrease += 0.25f;
+            }
+            else if ((int)fertilizer == 918) {
+                speedIncrease += 0.33f;
+            }
+            if (who.professions.Contains(5)) {
+                speedIncrease += 0.1f;
+            }
+            int daysToRemove = (int)Math.Ceiling((float)totalDaysOfCropGrowth * speedIncrease);
+            int tries = 0;
+            while (daysToRemove > 0 && tries < 3) {
+                for (int i = 0; i < crop.phaseDays.Count; i++) {
+                    if ((i > 0 || crop.phaseDays[i] > 1) && crop.phaseDays[i] != 99999) {
+                        crop.phaseDays[i]--;
+                        daysToRemove--;
+                    }
+                    if (daysToRemove <= 0) {
+                        break;
+                    }
+                }
+                tries++;
             }
         }
     }
