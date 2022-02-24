f88b8c4b3e76ca1ec93f86991da7eaed4ea2f1d3
ceruleandeep <ceruleandeep>
2022-02-24 16:43:47 +1100

    Crop map API support
    First used by Better Junimos Forestry for building hedge mazes, can be used elsewhere for custom crop patterns, custom field boundaries etc

--- BetterJunimos/BetterJunimosApi.cs
+++ BetterJunimos/BetterJunimosApi.cs
@@ -1,13 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using BetterJunimos.Abilities;
 using BetterJunimos.Utils;
-using StardewValley.Buildings;
 
 namespace BetterJunimos {
-    public class BetterJunimosApi {
+    public interface IBetterJunimosApi {
+        public int GetJunimoHutMaxRadius();
 
+        public int GetJunimoHutMaxJunimos();
+
+        public Dictionary<string, bool> GetJunimoAbilities();
+
+        public void RegisterJunimoAbility(IJunimoAbility junimoAbility);
+
+        public bool GetWereJunimosPaidToday();
+
+        public void ShowPerfectionTracker();
+        public void ListAvailableActions(Guid hutGuid);
+        public CropMap GetCropMapForHut(Guid hutGuid);
+        public void SetCropMapForHut(Guid hutGuid, CropMap map);
+        public void ClearCropMapForHut(Guid hutGuid);
+    }
+
+    public class BetterJunimosApi : IBetterJunimosApi {
         public int GetJunimoHutMaxRadius() {
-            return Util.Config.JunimoHuts.MaxRadius;
+            return Util.CurrentWorkingRadius;
         }
 
         public int GetJunimoHutMaxJunimos() {
@@ -15,7 +32,7 @@ namespace BetterJunimos {
         }
 
         public Dictionary<string, bool> GetJunimoAbilities() {
-            return Util.Config.JunimoAbilities;
+            return BetterJunimos.Config.JunimoAbilities;
         }
 
         public void RegisterJunimoAbility(IJunimoAbility junimoAbility) {
@@ -24,6 +41,26 @@ namespace BetterJunimos {
 
         public bool GetWereJunimosPaidToday() {
             return Util.Payments.WereJunimosPaidToday;
+        }
+
+        public void ShowPerfectionTracker() {
+            Util.Progression.ShowPerfectionTracker();
+        }
+
+        public void ListAvailableActions(Guid hutGuid) {
+            Util.Progression.ListAvailableActions(hutGuid);
+        }
+
+        public CropMap GetCropMapForHut(Guid hutGuid) {
+            return BetterJunimos.CropMaps.GetCropMapForHut(Util.GetHutFromId(hutGuid));
+        }
+
+        public void SetCropMapForHut(Guid hutGuid, CropMap map) {
+            BetterJunimos.CropMaps.SetCropMapForHut(Util.GetHutFromId(hutGuid), map);
+        }
+
+        public void ClearCropMapForHut(Guid hutGuid) {
+            BetterJunimos.CropMaps.ClearCropMapForHut(Util.GetHutFromId(hutGuid));
         }
     }
 }
