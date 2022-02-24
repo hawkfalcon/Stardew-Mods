56e4127d7175784a43bd0af27d13957953b37e78
ceruleandeep <ceruleandeep>
2022-02-24 16:45:16 +1100

    .NET 5 tidy-ups
    .NET 5 tidy-ups

--- BetterJunimos/JunimoEditor.cs
+++ BetterJunimos/JunimoEditor.cs
@@ -13,17 +13,27 @@ namespace BetterJunimos {
         }
 
         public bool CanEdit<T>(IAssetInfo asset) {
-            if (Util.Config.FunChanges.JunimosAlwaysHaveLeafUmbrellas || Game1.isRaining) {
+            if (BetterJunimos.Config.FunChanges.JunimosAlwaysHaveLeafUmbrellas || Game1.isRaining) {
+                return asset.AssetNameEquals(@"Characters\Junimo");
+            }
+            if (Game1.IsWinter) {
                 return asset.AssetNameEquals(@"Characters\Junimo");
             }
             return false;
         }
 
         public void Edit<T>(IAssetData asset) {
-            if (Util.Config.FunChanges.JunimosAlwaysHaveLeafUmbrellas || Game1.isRaining) {
+            if (BetterJunimos.Config.FunChanges.JunimosAlwaysHaveLeafUmbrellas || Game1.isRaining) {
                 Rectangle rectangle = new Rectangle(0, 0, 128, 128);
-                string umbrella = Util.Config.FunChanges.MoreColorfulLeafUmbrellas ? "JunimoUmbrellaOnly_Grayscale" : "JunimoUmbrellaOnly";
+                string umbrella = BetterJunimos.Config.FunChanges.MoreColorfulLeafUmbrellas ? "JunimoUmbrellaOnly_Grayscale" : "JunimoUmbrellaOnly";
                 Texture2D customTexture = Content.Load<Texture2D>("assets/" + umbrella + ".png", ContentSource.ModFolder);
+                asset.AsImage().PatchImage(customTexture, rectangle, rectangle, PatchMode.Overlay);
+                return;
+            }
+            if (Game1.IsWinter) {
+                Rectangle rectangle = new Rectangle(0, 0, 128, 128);
+                string beanie = "JunimoBeanie";
+                Texture2D customTexture = Content.Load<Texture2D>("assets/" + beanie + ".png", ContentSource.ModFolder);
                 asset.AsImage().PatchImage(customTexture, rectangle, rectangle, PatchMode.Overlay);
             }
         }
