cc6a93f95f967766cfd48645a749674c2cfa2784
hawkfalcon <contact@hawkfalcon.com>
2026-09-23 22:45:38 -0700

    Warn about infinite junimos setting

--- BetterJunimos/BetterJunimos.cs
+++ BetterJunimos/BetterJunimos.cs
@@ -251,6 +251,12 @@ namespace BetterJunimos {
             }
 
             if (huts.Any()) {
+                // players turn this on by mistake and then report items not being used up
+                if (Config.FunChanges.InfiniteJunimoInventory) {
+                    Monitor.LogOnce("FunChanges.InfiniteJunimoInventory is on: Junimos don't use up seeds, " +
+                                    "fertilizer, wages or ability items.", LogLevel.Info);
+                }
+
                 CheckHutsForWagesAndProgressionItems();
                 Util.Progression.DayStartedProgressionPrompt(Game1.IsWinter, Game1.isRaining);
                 JunimoAbilities.ResetCooldowns();
--- BetterJunimos/ModConfig.cs
+++ BetterJunimos/ModConfig.cs
@@ -55,6 +55,7 @@ namespace BetterJunimos {
             public float RainyJunimoSpiritFactor = 0.65f;
             public bool JunimosAlwaysHaveLeafUmbrellas { get; set; } = false;
             public bool MoreColorfulLeafUmbrellas { get; set; } = false;
+            // Cheat: Junimos never use up items from the hut chest; see Util.RemoveItemFromChest.
             public bool InfiniteJunimoInventory { get; set; } = false;
         }
 
